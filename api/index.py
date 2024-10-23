93bb96abeb4bb9be47ad28f368507c1f94b58bfb
hamzahis <164903490+hamzahis@users.noreply.github.com>
2024-10-23 23:26:08 +0100

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,12 +1,26 @@
-from flask import Flask, request, make_response
+from flask import Flask, request, make_response, render_template
 
 
 app = Flask(__name__)
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':   
 
-@app.route('/')
-def home():
-    public_ip = request.headers.get('Cookie')
-    return f"Your public IP is: {public_ip}"
+        name = request.form['name']
+        email = request.form['email']   
+
+        return f"Name: {name}<br>Email: {email}"
+    else:
+        return render_template('index.html')
+
+
+
+
+
+#@app.route('/')
+#def home():
+  #  public_ip = request.headers.get('Cookie')
+  #  return f"Your public IP is: {public_ip}"
  #   response = make_response('Hello, world!')
   #  response.set_cookie('yummy_cookie',   'chocolate')
   #  return response
