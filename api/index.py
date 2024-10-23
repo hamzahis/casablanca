c21d8dd1c2ece9996cf9a2856b1ae1739c314107
hamzahis <164903490+hamzahis@users.noreply.github.com>
2024-10-23 23:55:29 +0100

    Update index.py

--- api/index.py
+++ api/index.py
@@ -3,18 +3,6 @@ from flask import Flask, request, make_response, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    public_ip = request.headers.get('Cookie')
-    return f"Your public IP is: {public_ip}"
-  #  response = make_response('Hello, world!')
-  #  response.set_cookie('yummy_cookie',   'chocolate')
-  #  return response
-    
-
-
-
-"""
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':   
@@ -25,14 +13,20 @@ def index():
         return f"Name: {name}<br>Email: {email}"
     else:
         return render_template('index.html')
-        """
+        
 
 
 
 
 
+"""
 
-
-
-
+@app.route('/')
+def home():
+    public_ip = request.headers.get('Cookie')
+    return f"Your public IP is: {public_ip}"
+  #  response = make_response('Hello, world!')
+  #  response.set_cookie('yummy_cookie',   'chocolate')
+  #  return response
+"""
 
