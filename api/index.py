574f0e3bd43fe2e292863c74d5f178171f3e6b82
hamzahis <164903490+hamzahis@users.noreply.github.com>
2024-10-23 20:40:54 +0100

    Update index.py

--- api/index.py
+++ api/index.py
@@ -6,8 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     response = make_response('Hello, world!')
-    response.set_cookie('yummy_cookie',   
- 'chocolate')
+    response.set_cookie('yummy_cookie',   'chocolate')
     return response
   #  public_ip = request.headers.get('Cookie')
 #    return f"Your public IP is: {public_ip}"
