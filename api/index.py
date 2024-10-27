989cedfea7a26ba8cf351f7e25ca8a2580a8620a
hamzahis <164903490+hamzahis@users.noreply.github.com>
2024-10-27 21:43:53 +0100

    Update index.py

--- api/index.py
+++ api/index.py
@@ -3,11 +3,13 @@ from flask import Flask, request, make_response, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def get_public_ip():
-    # The 'X-Forwarded-For' header contains the public IP of the requester
-    public_ip = request.headers.get('Cookie')
-    return f"Your public IP is: {public_ip}"
+@app.route('/fetch-data')
+def fetch_data():
+    # Render the HTML template with the JavaScript fetch code
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
 
 
